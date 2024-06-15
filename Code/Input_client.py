fe8af1847fe0e196055eb86118b4a9f3d62a685b
MOBLOL <senya.ogurets1@mail.ru>
2024-06-15 19:37:07 +0300

    Architectori

--- Code/Input_client.py
+++ Code/Input_client.py
@@ -1,8 +1,8 @@
 import os
 import requests
 
-puth_history = "history/"
-puth_Chats = "Chats/"
+puth_history = "../history/"
+puth_Chats = "../Chats/"
 
 _Chats = "https://drive.google.com/drive/folders/1lT573WdwKYnyU4ds_2UbU79tGYZM30Gz?usp=drive_link"
 
